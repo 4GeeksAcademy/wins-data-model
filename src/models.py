38c30183e564e8debc9a61b965630c245867c011
Wins444 <winsking04yt@gmail.com>
2025-03-29 10:25:26 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,19 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id = mapped_column(Integer, primary_key=True)
+    username = mapped_column(String(50))
+    firstname = mapped_column(String(50))
+    lastname = mapped_column(String(50))
+    email = mapped_column(String(50), unique=True)
 
-
-    def serialize(self):
+def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Post(db.Model):
+    __tablename__ = "post"
+    def serialize(self):
+        return {
+        }
+    
+class Media(db.Model):
+    __tablename__ = "media"
+    def serialize(self):
+        return {
+        }
+    
+class Follower(db.Model):
+    __tablename__ = "follower"
+    def serialize(self):
+        return {
         }
